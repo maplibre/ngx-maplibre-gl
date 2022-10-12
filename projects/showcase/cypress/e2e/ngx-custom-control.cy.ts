2cb6a415eb8cef89614abc2d699c45ebed60ee0b
HarelM <harel.mazor@gmail.com>
2022-10-12 18:01:33 +0300

    First test passing

--- projects/showcase/cypress/e2e/ngx-custom-control.cy.ts
+++ projects/showcase/cypress/e2e/ngx-custom-control.cy.ts
@@ -1,12 +1,13 @@
+/// <reference types="cypress" />
 describe('Custom control', () => {
   it('should show custom control and pop up an alert box', () => {
     cy.visit('/demo/ngx-custom-control');
     cy.get('canvas').should('not.be.null');
     const button = cy.get('.custom-control');
-    button.should('have.text', 'Hello');
-    var alerted = false;
-    cy.on('window:alert', (_) => (alerted = true));
+    button.should('have.text', ' Hello ');
     button.click();
-    //expect(alerted).equal(true);
+    cy.on('window:alert', (text) => {
+      expect(text).to.contains('Hello');
+    });
   });
 });
