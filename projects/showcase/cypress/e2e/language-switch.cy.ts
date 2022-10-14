740b8e387c5b08a15d4d3995f5db2b2fdbf8c900
HarelM <harel.mazor@gmail.com>
2022-10-15 00:34:42 +0300

    try waiting more time...?

--- projects/showcase/cypress/e2e/language-switch.cy.ts
+++ projects/showcase/cypress/e2e/language-switch.cy.ts
@@ -7,12 +7,12 @@ describe('Language switch', () => {
     cy.visit('/demo/language-switch');
     cy.wait(2000); // wait for map to load
     cy.get('.lang-button').contains('French').click();
-    cy.wait(2000); // wait for language to change
+    cy.wait(4000); // wait for language to change
     driver.storeCanvasWidth();
     driver.storeCanvasHeight();
     driver.addImage();
     cy.get('.lang-button').contains('Russian').click();
-    cy.wait(2000); // wait for language to change
+    cy.wait(4000); // wait for language to change
     cy.get('canvas')
       .then((c) => {
         return pixelmatch(
@@ -25,7 +25,7 @@ describe('Language switch', () => {
       })
       .should('be.greaterThan', 0);
     cy.get('.lang-button').contains('French').click();
-    cy.wait(2000); // wait for language to change
+    cy.wait(4000); // wait for language to change
     cy.get('canvas')
       .then((c) => {
         return pixelmatch(
