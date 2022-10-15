e6b489542bd2f25d998f3b14be6f7c92a1705e70
HarelM <harel.mazor@gmail.com>
2022-10-15 21:18:48 +0300

    Small fixes

--- projects/showcase/cypress/e2e/language-switch.cy.ts
+++ projects/showcase/cypress/e2e/language-switch.cy.ts
@@ -6,13 +6,13 @@ describe('Language switch', () => {
     cy.visit('/demo/language-switch');
     cy.wait(2000); // wait for map to load
     cy.get('.lang-button').contains('French').click();
-    cy.wait(4000); // wait for language to change
+    cy.wait(7000); // wait for language to change
     driver.initReferenceImage();
     cy.get('.lang-button').contains('Russian').click();
-    cy.wait(4000); // wait for language to change
+    cy.wait(7000); // wait for language to change
     driver.compareToReference().should('be.greaterThan', 0);
     cy.get('.lang-button').contains('French').click();
-    cy.wait(4000); // wait for language to change
+    cy.wait(7000); // wait for language to change
     driver.compareToReference().should('equal', 0);
   });
 });
