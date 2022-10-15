14c29a8b149a00efc324319f4c6b1334c10fefec
HarelM <harel.mazor@gmail.com>
2022-10-15 18:48:22 +0300

    Fix test

--- projects/showcase/cypress/e2e/popup.cy.ts
+++ projects/showcase/cypress/e2e/popup.cy.ts
@@ -3,10 +3,9 @@ describe('Popup', () => {
     cy.visit('/demo/popup');
     cy.get('canvas').should('exist');
 
-    cy.get('.maplibregl-popup').within(() => {
-      cy.get('.custom-popup-class1').should('exist');
-      cy.get('.custom-popup-class2').should('exist');
-    });
+    cy.get('.maplibregl-popup').should('exist');
+    cy.get('.custom-popup-class1').should('exist');
+    cy.get('.custom-popup-class2').should('exist');
     cy.get('.maplibregl-popup-content').within(() => {
       cy.get('div').should('have.text', 'Hello world !');
     });
