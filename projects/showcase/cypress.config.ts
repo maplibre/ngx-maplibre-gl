7fb9424b1805ef94fac85da75354682eccc79a09
Harel M <harel.mazor@gmail.com>
2022-10-17 10:52:34 +0300

    Add retries to cypress tests

--- projects/showcase/cypress.config.ts
+++ projects/showcase/cypress.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    retries: {
+      runMode: 2
+    },
     baseUrl: 'http://localhost:4200',
     supportFile: 'projects/showcase/cypress/support/e2e.ts',
     specPattern: '**/*.cy.ts',
