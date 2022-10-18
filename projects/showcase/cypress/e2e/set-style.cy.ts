59c17fa15fd68ca67846a55c31ee8ca48d5cd45e
HarelM <harel.mazor@gmail.com>
2022-10-18 20:24:13 +0300

    Increase timeout to allow map to fully load

--- projects/showcase/cypress/e2e/set-style.cy.ts
+++ projects/showcase/cypress/e2e/set-style.cy.ts
@@ -5,14 +5,14 @@ describe('Set style', () => {
   it('should change the style', () => {
     console.warn = () => {};
     cy.visit('/demo/set-style');
-    cy.wait(5000);
+    cy.wait(20000);
     cy.get('canvas').should('exist');
     driver.initReferenceImage();
     cy.get('mat-radio-button').contains('from code').click();
-    cy.wait(5000);
+    cy.wait(10000);
     driver.compareToReference().should('be.greaterThan', 0);
     cy.get('mat-radio-button').contains('streets').click();
-    cy.wait(5000);
+    cy.wait(20000);
     cy.window().then((win) => {
       (win.console.warn as any).restore();
       cy.spy(win.console, 'warn');
