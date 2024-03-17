44a1d035ddfeecdd856878891d023a1cf2f2ddb6
HarelM <harel.mazor@gmail.com>
2024-03-17 12:22:38 +0000

    Deploying to gh-pages from @ maplibre/ngx-maplibre-gl@758ade0e85ab03bce4cf6d7d8516484a1c40a50d 🚀

---
+++ app/demo/examples/ngx-marker-rotate.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { MapComponent, MarkerComponent } from '@maplibre/ngx-maplibre-gl';
+
+@Component({
+  selector: 'showcase-demo',
+  template: `
+    <mgl-map
+      [style]="
+        'https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL'
+      "
+      [zoom]="[5]"
+      [center]="[144.946457, -37.840935]"
+      [preserveDrawingBuffer]="true"
+    >
+      <mgl-marker [lngLat]="[144.946457, -38.440935]" [rotation]="180"></mgl-marker>
+    </mgl-map>
+  `,
+  styleUrls: ['./examples.css'],
+  standalone: true,
+  imports: [MapComponent, MarkerComponent],
+})
+export class NgxMarkerRotateComponent {}
