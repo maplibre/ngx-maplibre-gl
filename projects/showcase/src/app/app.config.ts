b8d995822997f51be535a75fd2c88f287ff9b68c
Birk Skyum <birk.skyum@pm.me>
2024-03-28 04:59:17 +0100

    Enable SSR with migration script

--- projects/showcase/src/app/app.config.ts
+++ projects/showcase/src/app/app.config.ts
@@ -4,11 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    provideAnimations(),
+    provideAnimations(), provideClientHydration(), provideClientHydration(),
   ],
 };
