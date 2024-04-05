b8c5073fa906bd397a3874c4fea7112477e4ef12
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2024-04-05 12:14:01 +0200

    prerender with routes file

--- projects/showcase/src/app/demo/examples/live-update-feature.component.ts
+++ projects/showcase/src/app/demo/examples/live-update-feature.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, afterNextRender } from '@angular/core';
 import { LngLatLike } from 'maplibre-gl';
 import {
   MapComponent,
@@ -41,40 +41,42 @@ import { NgIf } from '@angular/common';
   standalone: true,
   imports: [MapComponent, NgIf, GeoJSONSourceComponent, LayerComponent],
 })
-export class LiveUpdateFeatureComponent implements OnInit, OnDestroy {
+export class LiveUpdateFeatureComponent implements OnDestroy {
   data: GeoJSON.FeatureCollection<GeoJSON.LineString>;
   center: LngLatLike;
   zoom = [0];
   pitch: number;
 
-  private timer: NodeJS.Timeout;
+  private timer?: NodeJS.Timeout;
 
-  constructor() {}
-
-  async ngOnInit() {
-    const data: GeoJSON.FeatureCollection<GeoJSON.LineString> = (await import(
-      './hike.geo.json'
-    )) as any;
-    const coordinates = data.features[0].geometry!.coordinates;
-    data.features[0].geometry!.coordinates = [coordinates[0]];
-    this.data = data;
-    this.center = <[number, number]>coordinates[0];
-    this.zoom = [14];
-    this.pitch = 30;
-    let i = 0;
-    this.timer = setInterval(() => {
-      if (i < coordinates.length) {
-        this.center = <[number, number]>coordinates[i];
-        data.features[0].geometry!.coordinates.push(coordinates[i]);
-        this.data = { ...this.data };
-        i++;
-      } else {
-        clearInterval(this.timer);
-      }
-    }, 10);
+  constructor() {
+    afterNextRender(async () => {
+      const data: GeoJSON.FeatureCollection<GeoJSON.LineString> = (await import(
+        './hike.geo.json'
+      )) as any;
+      const coordinates = data.features[0].geometry!.coordinates;
+      data.features[0].geometry!.coordinates = [coordinates[0]];
+      this.data = data;
+      this.center = <[number, number]>coordinates[0];
+      this.zoom = [14];
+      this.pitch = 30;
+      let i = 0;
+      this.timer = setInterval(() => {
+        if (i < coordinates.length) {
+          this.center = <[number, number]>coordinates[i];
+          data.features[0].geometry!.coordinates.push(coordinates[i]);
+          this.data = { ...this.data };
+          i++;
+        } else {
+          clearInterval(this.timer);
+        }
+      }, 10);
+    });
   }
 
   ngOnDestroy() {
-    clearInterval(this.timer);
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
   }
 }
