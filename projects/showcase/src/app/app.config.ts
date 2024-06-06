750bd85fe9c54fc0125045936ceb6413ee251ef5
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2024-06-06 10:13:23 +0200

    fix single quotes

--- projects/showcase/src/app/app.config.ts
+++ projects/showcase/src/app/app.config.ts
@@ -1,10 +1,10 @@
-import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
-import { provideRouter } from "@angular/router";
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
 
-import { routes } from "./app.routes";
-import { provideHttpClient, withFetch } from "@angular/common/http";
-import { provideAnimations } from "@angular/platform-browser/animations";
-import { provideClientHydration } from "@angular/platform-browser";
+import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
