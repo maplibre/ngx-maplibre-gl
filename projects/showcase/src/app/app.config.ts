797411d6385eced36b00544f23cbf6ae0a976755
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2024-06-06 10:03:42 +0200

    enable event coalescing
    * more info here https://angular.dev/api/core/NgZoneOptions

--- projects/showcase/src/app/app.config.ts
+++ projects/showcase/src/app/app.config.ts
@@ -1,13 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+import { provideRouter } from "@angular/router";
 
-import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideClientHydration } from '@angular/platform-browser';
+import { routes } from "./app.routes";
+import { provideHttpClient, withFetch } from "@angular/common/http";
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { provideClientHydration } from "@angular/platform-browser";
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withFetch()),
     provideAnimations(),
