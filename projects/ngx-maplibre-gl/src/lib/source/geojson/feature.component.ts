0f1e4dd0043226c30813a27755dbcef0b34a36c0
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2025-03-12 11:16:24 +0100

    add feature state methods to feature component

--- projects/ngx-maplibre-gl/src/lib/source/geojson/feature.component.ts
+++ projects/ngx-maplibre-gl/src/lib/source/geojson/feature.component.ts
@@ -59,4 +59,37 @@ export class FeatureComponent implements OnInit, OnDestroy {
     (<GeoJSON.Point>this.feature.geometry).coordinates = coordinates;
     this.geoJSONSourceComponent.updateFeatureData();
   }
+
+    /**
+     * Sets the `state` of a feature.
+     * 
+     * @param state A set of key-value pairs. The values should be valid JSON types.
+     * 
+     * @see [maplibre-gl-js' Docs](https://maplibre.org/maplibre-gl-js/docs/API/classes/Map/#setfeaturestate)
+     */
+    setFeatureState(state: any) {
+      this.geoJSONSourceComponent.setFeatureState({id: this.id()}, state);
+    }
+  
+    /**
+     * Removes the `state` of a feature, setting it back to the default behavior.
+     * 
+     * @param key The key in the feature state to reset.
+     * 
+     * @see [maplibre-gl-js' Docs](https://maplibre.org/maplibre-gl-js/docs/API/classes/Map/#removeFeatureState)
+     */
+    removeFeatureState(key?: string) {
+      this.geoJSONSourceComponent.removeFeatureState({id: this.id()}, key);
+    }
+  
+    /**
+     * Gets the `state` of a feature.
+     * 
+     * @returns The state of the feature: a set of key-value pairs that was assigned to the feature at runtime.
+     * 
+     * @see [maplibre-gl-js' Docs](https://maplibre.org/maplibre-gl-js/docs/API/classes/Map/#getFeatureState)
+     */
+    getFeatureState(): any {
+      return this.geoJSONSourceComponent.getFeatureState({id: this.id()});
+    }
 }
