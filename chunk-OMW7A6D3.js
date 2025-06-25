87c3f3c05126e8bb12998d68aaf22da8f74a3e52
HarelM <harel.mazor@gmail.com>
2025-06-25 06:53:23 +0000

    Deploying to gh-pages from @ maplibre/ngx-maplibre-gl@cbe4e54869eec7cb01d4d3e05c28515dfeb07e16 🚀

---
+++ chunk-OMW7A6D3.js
@@ -0,0 +1,2 @@
+import{l as i}from"./chunk-C3S7ABNN.js";import{$ as a,Ea as n,ba as t}from"./chunk-2WA6SCNZ.js";var e=new a("MATERIAL_ANIMATIONS");function c(){return t(e,{optional:!0})?.animationsDisabled||t(n,{optional:!0})==="NoopAnimations"?!0:t(i).matchMedia("(prefers-reduced-motion)").matches}export{c as a};
+//# sourceMappingURL=chunk-OMW7A6D3.js.map
