6c6abd713ac3c5a6ef8befd23a686be7fffd41c9
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-12-15 11:43:30 +0100

    fix: remove provideAnimations (package already removed)

--- projects/showcase/src/app/app.config.ts
+++ projects/showcase/src/app/app.config.ts
@@ -6,7 +6,6 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
@@ -14,7 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(withFetch()),
-    provideAnimations(),
     provideClientHydration(),
   ],
 };
